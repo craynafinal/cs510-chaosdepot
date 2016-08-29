9b4842fa3a1e119279ba40481b00ed50b24645e1
jsl <jsl@pdx.edu>
2016-08-29 01:13:22 -0700

    added another url for ping

--- ping.js
+++ ping.js
@@ -3,4 +3,5 @@ var http = require("http");
 // Requests get every 10 minutes
 setInterval(function() {
     http.get('http://chaosdepot.herokuapp.com');
+		http.get('http://chaosdepot.com');
 }, 600000);
